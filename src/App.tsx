31b24036e67bce492bb065f772279fc66358c040
Ricardo Sampaio <ricardosampaiomil@gmail.com>
2026-04-23 22:18:30 -0300

    style: inject dynamic glass glows into application background

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ import { useState, useEffect } from 'react';
 import { fetchTopCoins } from './services/api';
 import type { ICoin } from './types';
 import styles from './App.module.css';
+import glowTopGray from './assets/glows/glow top gray.svg';
+import glowTopGray1 from './assets/glows/glow top gray-1.svg';
+import glowBlackBottom from './assets/glows/glow black bottom.svg';
+import glowBlackBottom1 from './assets/glows/glow black bottom-1.svg';
+import glowBlackTop from './assets/glows/glow black top.svg';
+
 
 export default function App() {
   const [coins, setCoins] = useState<ICoin[]>([]);
@@ -35,10 +41,20 @@ export default function App() {
 
   return (
     <div className={styles.layout}>
-      {
-        
-      }
-      <h2>Moedas Carregadas com Sucesso: {coins.length}</h2>
+      {/* Background glows */}
+      <div className={styles['glow-container']}>
+        <img src={glowTopGray} className={styles['glow-1']} alt="" />
+        <img src={glowTopGray1} className={styles['glow-2']} alt="" />
+        <img src={glowBlackTop} className={styles['glow-3']} alt="" />
+        <img src={glowBlackBottom} className={styles['glow-4']} alt="" />
+        <img src={glowBlackBottom1} className={styles['glow-5']} alt="" />
+      </div>
+
+      {/* Teste temporario pra ver se a API conectou de verdade na tela preta */}
+      <h2 style={{ color: 'white', position: 'relative', zIndex: 1 }}>
+        Moedas Carregadas com Sucesso: {coins.length}
+      </h2>
     </div>
   );
+
 }
