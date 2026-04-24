c0bede0a9fdd93494227458e8659012b0ae60de7
Ricardo Sampaio <ricardosampaiomil@gmail.com>
2026-04-24 16:46:47 -0300

    style: setup Topbar flexbox dimensions and spacing

--- src/components/layout/Topbar/Topbar.tsx
+++ src/components/layout/Topbar/Topbar.tsx
@@ -1,22 +1,21 @@
 import { Search } from 'lucide-react';
+import styles from './Topbar.module.css';
 import logoBeacon from '../../../assets/images/logo-beacon.webp';
 import logoAvatar from '../../../assets/images/logo-avatar.webp';
 
 export const Topbar = () => {
   return (
-    <header>
-      <div>
+    <header className={styles.header}>
+      <div className={styles.container}>
         
-        {/* Bloco da Logo */}
-        <div>
-          <div>
+        <div className={styles.logo}>
+          <div className={styles['logo-icon']}>
             <img src={logoBeacon} alt="Logo" style={{ width: '100%', height: '100%', objectFit: 'contain' }} />
           </div>
-          <h1>Beacon</h1>
+          <h1 className={styles['logo-text']}>Beacon</h1>
         </div>
 
-        {/* Bloco de Pesquisa */}
-        <div>
+        <div className={styles['search-bar']}>
           <Search size={24} color="#6d6d6d" />
           <input 
             type="text" 
@@ -26,9 +25,8 @@ export const Topbar = () => {
 
       </div>
 
-      {/* Bloco do Usuário */}
-      <div>
-        <img src={logoAvatar} alt="Avatar" />
+      <div className={styles.avatar}>
+        <img src={logoAvatar} alt="Avatar" className={styles['avatar-image']} />
       </div>
     </header>
   );
