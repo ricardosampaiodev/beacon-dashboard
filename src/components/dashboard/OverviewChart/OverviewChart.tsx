dce0c4f0ff5d956316d5440975c48e306b285fd3
Ricardo Sampaio <ricardosampaiomil@gmail.com>
2026-04-27 18:27:21 -0300

    style: apply dynamic tooltip styling for hover properties

--- src/components/dashboard/OverviewChart/OverviewChart.tsx
+++ src/components/dashboard/OverviewChart/OverviewChart.tsx
@@ -1,5 +1,5 @@
 import { useMemo } from 'react';
-import { AreaChart, Area, ResponsiveContainer, XAxis } from 'recharts';
+import { AreaChart, Area, ResponsiveContainer, Tooltip, XAxis } from 'recharts';
 import type { ICoin } from '../../../types';
 import styles from './OverviewChart.module.css';
 
@@ -31,14 +31,34 @@ export const OverviewChart: React.FC<OverviewChartProps> = ({ selectedCoin }) =>
           <h3 className={styles['coin-name']}>{selectedCoin ? selectedCoin.name : 'Selecione uma moeda'}</h3>
           <span className={styles['overview-text']}>Visão Geral</span>
         </div>
+        
         <div className={styles['avg-text']}>Média Mensal</div>
       </div>
 
       <div className={styles['chart-wrapper']}>
         <ResponsiveContainer width="100%" height="100%">
-          <AreaChart data={chartData}>
-            <XAxis dataKey="name" />
-            <Area type="monotone" dataKey="val" stroke="#4c8ff8" fill="#4c8ff8" />
+          <AreaChart data={chartData} margin={{ top: 10, right: 15, left: 15, bottom: 0 }}>
+            <defs>
+              <linearGradient id="colorVal" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#4c8ff8" stopOpacity={0.3}/>
+                <stop offset="95%" stopColor="#4c8ff8" stopOpacity={0}/>
+              </linearGradient>
+            </defs>
+            <Tooltip 
+              contentStyle={{ background: '#0e0f11', border: '1px solid #5f646d', borderRadius: '8px' }}
+              itemStyle={{ color: '#e9e9e9' }}
+              labelStyle={{ display: 'none' }}
+              formatter={(value: any) => [`$${Number(value).toFixed(2)}`, 'Preço']}
+            />
+            <XAxis dataKey="name" stroke="#7b7b7b" axisLine={false} tickLine={false} tick={{ fontSize: 14, fontFamily: 'Poppins' }} dy={10} interval={0} />
+            <Area 
+              type="monotone" 
+              dataKey="val" 
+              stroke="#4c8ff8" 
+              strokeWidth={3}
+              fillOpacity={1} 
+              fill="url(#colorVal)" 
+            />
           </AreaChart>
         </ResponsiveContainer>
       </div>
